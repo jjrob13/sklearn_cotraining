52f6bb9110e58efb8832d84bbd22deb3e541412b
John Robertson <jj.robertson@asu.edu>
2016-03-26 09:46:16 -0700

    Change version number to 0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Scikit Cotraining',
-    version='0.1dev',
+    version='0.1',
     packages=['cotraining',],
     long_description=open('README.md').read(),
 )
