2654ca1b75727f828bef236c22b1be01e2fea255
John Robertson <jj.robertson@asu.edu>
2016-03-26 10:14:43 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='sklearn_cotraining',
-    version='0.1dev',
+    version='0.1',
     packages=['sklearn_cotraining',],
     long_description=open('README.md').read(),
 )
